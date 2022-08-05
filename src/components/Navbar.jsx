32743879b3a0f552101c8390e602fa3bdbef8cd2
ol3gatron <olegatron@gmail.com>
2022-08-05 18:37:24 +0300

    created pages and routes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import {useNavigate, useLocation} from "react-router-dom"
+
+function Navbar() {
+  return (
+    <div>Navbar</div>
+  )
+}
+export default Navbar
---
+++ src/pages/SignIn.jsx
@@ -0,0 +1,6 @@
+function SignIn() {
+  return (
+    <div>Sign In</div>
+  )
+}
+export default SignIn
