b95ef7130265894980e59eda6ebaf4dcfb4848bf
ol3gatron <olegatron@gmail.com>
2022-11-17 12:15:26 +0300

    listing details

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -30,8 +30,58 @@ const Listing = () => {
     fetchListing()
   }, [navigate, params.listingId])
 
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
-    <div>Listing</div>
+    <main>
+      <div className="shareIconDiv" onClick={() => {
+        navigator.clipboard.writeText(window.location.href)
+        setShareLinkCopied(true)
+        setTimeout(() => {
+          setShareLinkCopied(false)
+        }, 2000)
+      }}>
+        <img src={ShareIcon} alt="" />
+      </div>
+
+      {shareLinkCopied && <p className="linkCopied">Link Copied!</p>}
+
+      <div className="listingDetails">
+        <p className="listingName">{listing.name} - $
+        {listing.offer.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") ? listing.discountedPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") : listing.regularPrice.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+        </p>
+        <div className="listingLocation">{listing.location}</div>
+        <p className="listingType">
+          For {listing.type === "rent" ? "Rent" : "Sale"}
+        </p>
+        {listing.offer && (
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
+        )}
+
+        <ul className="listingDetailsList">
+          <li>
+            {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
+        </ul>
+
+        <p className="listingLocationTitle">Location</p>
+
+        {auth.currentUser?.uid !== listing.userRef && (
+          <Link to={`/contact/${listing.userRef}?listingName=${listing.name}&listingLocation=${listing.location}`} className="primaryButton">
+            Contact Landlord
+          </Link>
+        )}
+      </div>
+    </main>
   )
 }
 export default Listing
